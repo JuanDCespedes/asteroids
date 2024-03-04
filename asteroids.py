882283dc15b0a8b28e65f96f0a8a2d78d8c89d3f
JuanDCespedes <jdcu2005@gmail.com>
2024-03-04 07:50:57 -0500

    act

--- asteroids.py
+++ asteroids.py
@@ -6,6 +6,9 @@ size=width, height=800, 600
 screen=pygame.display.set_mode(size)
 def main():
     pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load("sonido/outer.mp3")
+    pygame.mixer.music.play(1)
     background_image=pygame.image.load("imagenes/space.png")
     background_rect=background_image.get_rect()
     pygame.display.set_caption("Asteroids")
@@ -14,8 +17,13 @@ def main():
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 sys.exit()
-        ship.update()
         screen.blit(background_image, background_rect)
+        ship.update()
+        for bullet in ship.bullets:
+            bullet.update()
+            if bullet.alcance ==0:
+                ship.bullets.remove(bullet)
+            screen.blit(bullet.image, bullet.rect)
         screen.blit(ship.imagen, ship.rect)
         pygame.display.update()
         pygame.time.delay(10)
